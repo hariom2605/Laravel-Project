0fd2782c90100c9ef9ebcb126d61ec195ebaf882
hariom2605 <dixitabhishek918@gmail.com>
2023-05-15 16:48:00 +0530

    update

---
+++ resources/views/master.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commrce Project</title>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
+</head>
+<body>
+    <style>
+        .form-cus 
+        {
+          height:500px;  
+        }
+        </style>
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
+    
+</body>
+<!-- <script>
+$(document).ready(function()
+{
+$("button").click(function()
+{
+alert('hellow');
+
+})
+
+})
+    </script> -->
+</html>
